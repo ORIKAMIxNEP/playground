df96204032211cc22c1c5a61b953a20b22c6979b
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-14 12:58:20 +0900

    Update and rename AddRequest.java to AddInput.java

--- application/src/main/java/jp/spring_boot_template/application/dto/record/AddInput.java
+++ application/src/main/java/jp/spring_boot_template/application/dto/record/AddInput.java
@@ -3,4 +3,4 @@ package jp.spring_boot_template.application.dto.record;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record AddRequest(short record1, @NotBlank @Size(max = 10) String record2) {}
+public record AddInput(short record1, @NotBlank @Size(max = 10) String record2) {}
