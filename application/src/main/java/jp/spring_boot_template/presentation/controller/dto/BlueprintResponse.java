b7d8ebbb169a6d629975c5d03a5e7ebce30fc252
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-15 12:51:50 +0900

    Create BlueprintResponse.java

---
+++ application/src/main/java/jp/spring_boot_template/presentation/controller/dto/BlueprintResponse.java
@@ -0,0 +1,8 @@
+package jp.spring_boot_template.presentation.controller.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+@Schema(name = "ステータス")
+@Builder
+public record BlueprintResponse(@Schema(name = "成功", type = "boolean" example = true) boolean success) {}
