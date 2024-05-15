8d91dbae5a6c991cc709e5a40c89263b2bbcabe7
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-15 11:04:41 +0900

    Update AddRequest.java

--- application/src/main/java/jp/spring_boot_template/presentation/controller/dto/AddRequest.java
+++ application/src/main/java/jp/spring_boot_template/presentation/controller/dto/AddRequest.java
@@ -6,4 +6,4 @@ import lombok.Builder;
 
 @Schema(description = "レコード")
 @Builder
-public record AddRequest(@Schema(description = "カラム1", type = "integer", format = "int32", minimun = 0, maximum = 127, example = 0) byte column1, @NotBlank @Size(max = 10) @Schema(description = "カラム2", type = "string", minLength = 1, maxLength = 10, example = "a") String column2) {}
+public record AddRequest(@Schema(name = "カラム1", type = "integer", format = "int32", minimun = 0, maximum = 127, example = 0, required = true) byte column1, @NotBlank @Size(max = 10) @Schema(name = "カラム2", type = "string", minLength = 1, maxLength = 10, example = "a", required = true) String column2) {}
