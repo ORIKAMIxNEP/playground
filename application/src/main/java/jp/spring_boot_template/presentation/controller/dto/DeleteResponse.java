f4d1f8b1d2010c5f85a1baa29eed49e28ae61246
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-15 11:26:27 +0900

    Update DeleteResponse.java

--- application/src/main/java/jp/spring_boot_template/presentation/controller/dto/DeleteResponse.java
+++ application/src/main/java/jp/spring_boot_template/presentation/controller/dto/DeleteResponse.java
@@ -3,6 +3,6 @@ package jp.spring_boot_template.presentation.controller.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
-@Schema(description = "ステータス")
+@Schema(name = "ステータス")
 @Builder
 public record DeleteResponse(@Schema(name = "成功", type = "boolean" example = true) boolean success) {}
