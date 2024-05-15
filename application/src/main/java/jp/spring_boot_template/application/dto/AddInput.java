108bdb8b3366ea9f458622b267830960aa557a87
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-15 11:32:10 +0900

    Update AddInput.java

--- application/src/main/java/jp/spring_boot_template/application/dto/AddInput.java
+++ application/src/main/java/jp/spring_boot_template/application/dto/AddInput.java
@@ -3,4 +3,4 @@ package jp.spring_boot_template.application.dto;
 import lombok.Builder;
 
 @Builder
-public record AddInput(short column1, String column2) {}
+public record AddInput(byte column1, String column2) {}
