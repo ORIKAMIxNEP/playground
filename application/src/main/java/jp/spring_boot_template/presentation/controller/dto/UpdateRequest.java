86528bb45934e83d882ae699caf7ef0f399f3ce8
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-15 11:30:04 +0900

    Update UpdateRequest.java

--- application/src/main/java/jp/spring_boot_template/presentation/controller/dto/UpdateRequest.java
+++ application/src/main/java/jp/spring_boot_template/presentation/controller/dto/UpdateRequest.java
@@ -2,5 +2,6 @@ package jp.spring_boot_template.presentation.controller.dto;
 
 import lombok.Builder;
 
+@Schema(name = "レコード")
 @Builder
-public record UpdateRequest(long recordId, short column1, String column2) {}
+public record UpdateRequest(@Schema(name = "レコードID", type = "integer", format = "int64", minimun = 1, example = 1, required = true) long recordId, @Schema(name = "カラム1", type = "integer", format = "int32", minimun = 0, maximum = 127, example = 0, required = true) short column1, @NotBlank @Size(max = 10) @Schema(name = "カラム2", type = "string", minLength = 1, maxLength = 10, example = "a", required = true) String column2) {}
