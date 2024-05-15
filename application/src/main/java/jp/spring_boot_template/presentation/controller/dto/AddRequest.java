e968f8f17e0037a34bba0512f060f100e28790d8
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-15 11:24:14 +0900

    Update AddRequest.java

--- application/src/main/java/jp/spring_boot_template/presentation/controller/dto/AddRequest.java
+++ application/src/main/java/jp/spring_boot_template/presentation/controller/dto/AddRequest.java
@@ -4,6 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
-@Schema(description = "レコード")
+@Schema(name = "レコード")
 @Builder
 public record AddRequest(@Schema(name = "カラム1", type = "integer", format = "int32", minimun = 0, maximum = 127, example = 0, required = true) byte column1, @NotBlank @Size(max = 10) @Schema(name = "カラム2", type = "string", minLength = 1, maxLength = 10, example = "a", required = true) String column2) {}
