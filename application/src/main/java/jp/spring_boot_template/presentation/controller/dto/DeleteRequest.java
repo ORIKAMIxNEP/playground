5cf946a70f89769c79dd09d66631bc4eeb82206e
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-15 11:16:48 +0900

    Update DeleteRequest.java

--- application/src/main/java/jp/spring_boot_template/presentation/controller/dto/DeleteRequest.java
+++ application/src/main/java/jp/spring_boot_template/presentation/controller/dto/DeleteRequest.java
@@ -3,4 +3,4 @@ package jp.spring_boot_template.presentation.controller.dto;
 import lombok.Builder;
 
 @Builder
-public record DeleteRequest(long recordId) {}
+public record DeleteRequest(@Schema(name = "レコードID", type = "integer", format = "int64", minimun = 1, example = 1, required = true) long recordId) {}
