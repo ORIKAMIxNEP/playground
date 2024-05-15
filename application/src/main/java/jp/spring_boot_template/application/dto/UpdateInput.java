dba670e91307d11a975b6ebb71dcf4c2b8a61f84
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-15 11:33:16 +0900

    Update UpdateInput.java

--- application/src/main/java/jp/spring_boot_template/application/dto/UpdateInput.java
+++ application/src/main/java/jp/spring_boot_template/application/dto/UpdateInput.java
@@ -3,4 +3,4 @@ package jp.spring_boot_template.application.dto;
 import lombok.Builder;
 
 @Builder
-public record UpdateInput(long recordId, short column1, String column2) {}
+public record UpdateInput(long recordId, byte column1, String column2) {}
