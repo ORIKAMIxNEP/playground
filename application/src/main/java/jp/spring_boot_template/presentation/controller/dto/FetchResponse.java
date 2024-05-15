6920a0cc70987fd5881394f8eea06d3afdb084e3
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-15 11:39:12 +0900

    Update FetchResponse.java

--- application/src/main/java/jp/spring_boot_template/presentation/controller/dto/FetchResponse.java
+++ application/src/main/java/jp/spring_boot_template/presentation/controller/dto/FetchResponse.java
@@ -6,4 +6,4 @@ import lombok.Builder;
 @Schema(name = "レコード")
 @Builder
 public record FetchResponse(
-    @Schema(name = "カラム1", type = "integer", format = "int32", minimun = 0, maximum = 127, nullable = true, example = 0) Byte column1, @Schema(name = "カラム2", type = "string", minLength = 1, maxLength = 10, example = "a") String column2) {}
+    @Schema(name = "カラム1", type = "integer", format = "int32", minimun = 0, maximum = 127, nullable = true, example = 0) Byte column1, @Schema(name = "カラム2", type = "string", minLength = 1, maxLength = 10, nullable = true, example = "a") String column2) {}
