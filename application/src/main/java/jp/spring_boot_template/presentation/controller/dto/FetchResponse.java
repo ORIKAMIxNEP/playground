fff4a55cc3842ebc217101311933a2537fcba5b5
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-15 11:27:07 +0900

    Update FetchResponse.java

--- application/src/main/java/jp/spring_boot_template/presentation/controller/dto/FetchResponse.java
+++ application/src/main/java/jp/spring_boot_template/presentation/controller/dto/FetchResponse.java
@@ -3,7 +3,7 @@ package jp.spring_boot_template.presentation.controller.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
-@Schema(description = "レコード")
+@Schema(name = "レコード")
 @Builder
 public record FetchResponse(
     @Schema(name = "カラム1", type = "integer", format = "int32", minimun = 0, maximum = 127, example = 0) byte column1, @Schema(name = "カラム2", type = "string", minLength = 1, maxLength = 10, example = "a") String column2) {}
