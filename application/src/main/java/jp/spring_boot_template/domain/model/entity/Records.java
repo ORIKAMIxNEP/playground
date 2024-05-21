e2e6ecbab28e378494306d4c3caa3946a184913b
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-21 15:31:10 +0900

    Update Records.java

--- application/src/main/java/jp/spring_boot_template/domain/model/entity/Records.java
+++ application/src/main/java/jp/spring_boot_template/domain/model/entity/Records.java
@@ -1,15 +1,12 @@
 package jp.spring_boot_template.domain.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Data;
 
-@Entity
 @Data
 @Builder
 public class Records {
-  @Id private final long recordId;
+  private final long recordId;
 
   private final byte column1;
 
