86ea477c6a89513b87090fbf2e620f82f412e96d
ORIKAMIxNEP <taiki.orikami@gmail.com>
2024-05-22 07:33:47 +0900

    Update

--- application/src/main/java/jp/spring_boot_template/presentation/error_handler/HttpClientErrorHandlerResponse.java
+++
@@ -1,7 +0,0 @@
-package com.spring_boot_template.presentation.error_handler;
-
-import lombok.Builder;
-import org.springframework.http.ResponseEntity;
-
-@Builder
-public record HttpClientErrorHandlerResponse(boolean error, ResponseEntity<?> responseEntity) {}
--- application/src/main/java/jp/spring_boot_template/presentation/record/response/UpdateRecordResponse.java
+++
@@ -1,9 +0,0 @@
-package com.spring_boot_template.presentation.record.response;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
-
-@Schema(description = "更新が成功したか")
-@Builder
-public record UpdateRecordResponse(
-    @Schema(title = "成功", type = "boolean", example = "true") boolean success) {}
