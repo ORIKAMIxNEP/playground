a4392e0924bce33d814b8d8ccbf0b2437585a261
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-22 16:19:11 +0900

    Update FetchRecordUseCase.java

--- application/src/main/java/com/spring_boot_template/application/record/FetchRecordUseCase.java
+++ application/src/main/java/com/spring_boot_template/application/record/FetchRecordUseCase.java
@@ -1,8 +1,9 @@
 package com.spring_boot_template.application.record;
 
+import com.spring_boot_template.presentation.record.request.FetchRecordRequest;
 import com.spring_boot_template.presentation.record.response.FetchRecordResponse;
 
 public interface FetchRecordUseCase {
   // レコード取得
-  FetchRecordResponse fetchRecord();
+  FetchRecordResponse execute(final FetchRecordRequest fetchRecordRequest);
 }
