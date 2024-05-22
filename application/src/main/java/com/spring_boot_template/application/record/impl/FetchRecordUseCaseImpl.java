413db6dae7792081450bdf9b42de9b608092559f
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-22 16:21:44 +0900

    Update FetchRecordUseCaseImpl.java

--- application/src/main/java/com/spring_boot_template/application/record/impl/FetchRecordUseCaseImpl.java
+++ application/src/main/java/com/spring_boot_template/application/record/impl/FetchRecordUseCaseImpl.java
@@ -3,6 +3,7 @@ package com.spring_boot_template.application.record.impl;
 import com.spring_boot_template.application.record.FetchRecordUseCase;
 import com.spring_boot_template.domain.record.Record;
 import com.spring_boot_template.infrastructure.record.RecordRdbRepository;
+import com.spring_boot_template.presentation.record.request.FetchRecordRequest;
 import com.spring_boot_template.presentation.record.response.FetchRecordResponse;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
@@ -11,23 +12,24 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class FetchRecordUseCaseImpl implements FetchRecordUseCase {
   private final RecordRdbRepository recordRdbRepository;
 
   // レコード取得
-  public FetchRecordResponse fetchRecord() {
-    final Record record = recordRdbRepository.fetchRecord(1);
+  @Transactional
+  public FetchRecordResponse execute(final FetchRecordRequest fetchRecordRequest) {
+    final long recordId = fetchRecordRequest.recordId();
+    final Record recordInput = Record.builder().recordId(recordId).build();
+    final Record recordOutput = recordRdbRepository.fetchRecord(recordInput);
 
     // レコードが存在しない場合
-    if (Objects.isNull(record)) {
-      return FetchRecordResponse.builder().success(false).column1(null).column2(null).build();
+    if (Objects.isNull(recordOutput)) {
+      return FetchRecordResponse.builder().successful(false).column1(null).column2(null).build();
     }
 
-    return FetchRecordResponse.builder()
-        .success(true)
-        .column1(record.getColumn1())
-        .column2(record.getColumn2())
-        .build();
+    final byte column1 = recordOutput.column1();
+    final String column2 = recordOutput.column2();
+
+    return FetchRecordResponse.builder().successful(true).column1(column1).column2(column2).build();
   }
 }
