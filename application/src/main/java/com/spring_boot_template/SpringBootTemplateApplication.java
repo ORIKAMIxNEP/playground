7ca62a84ff34d0a464c3626dc07c1e0f6e281989
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-28 12:59:00 +0900

    Update SpringBootTemplateApplication.java

--- application/src/main/java/com/spring_boot_template/SpringBootTemplateApplication.java
+++ application/src/main/java/com/spring_boot_template/SpringBootTemplateApplication.java
@@ -7,13 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @OpenAPIDefinition(
-    info =
-        @Info(
-            title = "Spring Boot Template",
-            version = "1.0",
-            description = "Spring Boot Template"))
+        info =
+                @Info(
+                        title = "Spring Boot Template",
+                        version = "1.0",
+                        description = "Spring Boot Template"))
 public class SpringBootTemplateApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(SpringBootTemplateApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTemplateApplication.class, args);
+    }
 }
