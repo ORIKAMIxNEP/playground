20713305147e4b6741a7714aa442ee76753a6057
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-28 14:02:04 +0900

    Update and rename application/src/main/java/com/spring_boot_template/presentation/record/request/UpdateRecordRequest.java to application/src/main/java/com/spring_boot_template/presentation/controller/record/request/UpdateRecordRequest.java

---
+++ application/src/main/java/com/spring_boot_template/presentation/controller/record/request/UpdateRecordRequest.java
@@ -0,0 +1,20 @@
+package com.spring_boot_template.presentation.controller.record.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(
+        description = "更新するレコードのデータ",
+        requiredProperties = {"recordId", "column1", "column2"})
+public record UpdateRecordRequest(
+        @Schema(title = "レコードID", type = "integer", format = "int64", minimum = "1", example = "1")
+                long recordId,
+        @Schema(
+                        title = "レコードカラム1",
+                        type = "integer",
+                        format = "int32",
+                        maximum = "127",
+                        minimum = "0",
+                        example = "0")
+                Byte column1,
+        @Schema(title = "レコードカラム2", type = "string", maxLength = 10, example = "a")
+                String column2) {}
