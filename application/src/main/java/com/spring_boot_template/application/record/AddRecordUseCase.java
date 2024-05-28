c9999e3d2f28af600d69d14bfa28c7702d39bd83
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-28 14:45:29 +0900

    Update and rename application/src/main/java/com/spring_boot_template/application/record/AddRecordUseCase.java to application/src/main/java/com/spring_boot_template/application/usecase/record/AddRecordUseCase.java

--- application/src/main/java/com/spring_boot_template/application/record/AddRecordUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.spring_boot_template.application.record;
-
-import com.spring_boot_template.presentation.record.request.AddRecordRequest;
-import com.spring_boot_template.presentation.record.response.AddRecordResponse;
-
-public interface AddRecordUseCase {
-  AddRecordResponse execute(final AddRecordRequest addRecordRequest);
-}
