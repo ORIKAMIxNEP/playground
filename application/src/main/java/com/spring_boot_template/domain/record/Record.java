3536ad925e2d3e8b35429d0d31f726562c651000
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-28 11:17:23 +0900

    Update Record.java

--- application/src/main/java/com/spring_boot_template/domain/record/Record.java
+++ application/src/main/java/com/spring_boot_template/domain/record/Record.java
@@ -1,12 +1,20 @@
 package com.spring_boot_template.domain.record;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-@Data
-@Builder
-public class Record {
-  private final RecordId recordId;
-  private final byte column1;
-  private final String column2;
+@Getter
+@RequiredArgsConstructor
+public class RecordEntity {
+    private final RecordIdValue recordId;
+    private final Column1Value column1;
+    private final Column2Value column2;
+
+    public RecordEntity(final RecordIdValue recordIdValue) {
+        this(recordIdValue, null, null);
+    }
+
+    public RecordEntity(final Column1Value column1Value, final Column2Value column2Value) {
+        this(null, column1Value, column2Value);
+    }
 }
