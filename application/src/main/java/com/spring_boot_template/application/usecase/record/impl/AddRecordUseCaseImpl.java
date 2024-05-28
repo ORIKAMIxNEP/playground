f2458fdca6bcd451290ee8e9d17be77e03863496
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-28 14:52:44 +0900

    Update and rename application/src/main/java/com/spring_boot_template/application/record/impl/AddRecordUseCaseImpl.java to application/src/main/java/com/spring_boot_template/application/usecase/record/impl/AddRecordUseCaseImpl.java

---
+++ application/src/main/java/com/spring_boot_template/application/usecase/record/impl/AddRecordUseCaseImpl.java
@@ -0,0 +1,28 @@
+package com.spring_boot_template.application.usecase.record.impl;
+
+import com.spring_boot_template.application.usecase.record.AddRecordUseCase;
+import com.spring_boot_template.domain.model.record.Column1Value;
+import com.spring_boot_template.domain.model.record.Column2Value;
+import com.spring_boot_template.domain.model.record.RecordEntity;
+import com.spring_boot_template.infrastructure.record.RecordRdbRepository;
+import com.spring_boot_template.presentation.controller.record.request.AddRecordRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AddRecordUseCaseImpl implements AddRecordUseCase {
+    private final RecordRdbRepository recordRdbRepository;
+
+    @Transactional
+    public void execute(final AddRecordRequest addRecordRequest) {
+        final byte column1 = addRecordRequest.column1();
+        final Column1Value column1Value = new Column1Value(column1);
+        final String column2 = addRecordRequest.column2();
+        final Column2Value column2Value = new Column2Value(column2);
+        final RecordEntity recordEntity = new RecordEntity(column1Value, column2Value);
+
+        recordRdbRepository.addRecord(recordEntity);
+    }
+}
