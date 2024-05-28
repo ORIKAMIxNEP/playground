719ff27174c1080ce6e4f459030cede1dd990c88
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-28 14:45:50 +0900

    Update DeleteRecordUseCase.java

--- application/src/main/java/com/spring_boot_template/application/record/DeleteRecordUseCase.java
+++ application/src/main/java/com/spring_boot_template/application/record/DeleteRecordUseCase.java
@@ -1,8 +1,7 @@
-package com.spring_boot_template.application.record;
+package com.spring_boot_template.application.usecase.record;
 
-import com.spring_boot_template.presentation.record.request.DeleteRecordRequest;
-import com.spring_boot_template.presentation.record.response.DeleteRecordResponse;
+import com.spring_boot_template.presentation.controller.record.request.DeleteRecordRequest;
 
 public interface DeleteRecordUseCase {
-  DeleteRecordResponse execute(final DeleteRecordRequest deleteRecordRequest);
+    void execute(final DeleteRecordRequest deleteRecordRequest);
 }
