028ef24b07387622c5a613ea57a078c622b5d90b
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-28 14:46:26 +0900

    Update ExistsRecordUseCase.java

--- application/src/main/java/com/spring_boot_template/application/record/ExistsRecordUseCase.java
+++ application/src/main/java/com/spring_boot_template/application/record/ExistsRecordUseCase.java
@@ -1,5 +1,7 @@
-package com.spring_boot_template.application.record;
+package com.spring_boot_template.application.usecase.record;
+
+import com.spring_boot_template.domain.model.record.RecordIdValue;
 
 public interface ExistsRecordUseCase {
-  boolean execute(final long recordId);
+    void execute(final RecordIdValue recordIdValue);
 }
