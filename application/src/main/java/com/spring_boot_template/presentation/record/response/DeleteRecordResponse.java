242b125ec7716e897b918b5de77836536fa6a1c7
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-28 13:44:53 +0900

    Delete application/src/main/java/com/spring_boot_template/presentation/record/response/DeleteRecordResponse.java

--- application/src/main/java/com/spring_boot_template/presentation/record/response/DeleteRecordResponse.java
+++
@@ -1,9 +0,0 @@
-package com.spring_boot_template.presentation.record.response;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
-
-@Schema(description = "削除が成功したか")
-@Builder
-public record DeleteRecordResponse(
-    @Schema(title = "成功したか", type = "boolean", example = "true") boolean isSuccessful) {}
