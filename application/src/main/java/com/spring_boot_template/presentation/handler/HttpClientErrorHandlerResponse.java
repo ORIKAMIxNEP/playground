9195a7fa27996ba38a668362cbf916c3eb0e7406
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-28 13:00:20 +0900

    Delete application/src/main/java/com/spring_boot_template/presentation/handler/HttpClientErrorHandlerResponse.java

--- application/src/main/java/com/spring_boot_template/presentation/handler/HttpClientErrorHandlerResponse.java
+++
@@ -1,7 +0,0 @@
-package com.spring_boot_template.presentation.handler;
-
-import lombok.Builder;
-import org.springframework.http.ResponseEntity;
-
-@Builder
-public record HttpClientErrorHandlerResponse(boolean hasError, ResponseEntity<?> responseEntity) {}
