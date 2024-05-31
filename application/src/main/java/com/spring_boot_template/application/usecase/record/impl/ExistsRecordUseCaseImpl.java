2d11c7e5fc081cceab0935c48e488b3f2fa479ce
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-31 16:55:00 +0900

    Update ExistsRecordUseCaseImpl.java

--- application/src/main/java/com/spring_boot_template/application/usecase/record/impl/ExistsRecordUseCaseImpl.java
+++ application/src/main/java/com/spring_boot_template/application/usecase/record/impl/ExistsRecordUseCaseImpl.java
@@ -4,7 +4,7 @@ import com.spring_boot_template.application.usecase.record.ExistsRecordUseCase;
 import com.spring_boot_template.domain.exception.ValidationException;
 import com.spring_boot_template.domain.model.record.RecordIdValue;
 import com.spring_boot_template.infrastructure.record.RecordRdbRepository;
-import java.util.Objects;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,11 +14,10 @@ import org.springframework.transaction.annotation.Transactional;
 class ExistsRecordUseCaseImpl implements ExistsRecordUseCase {
     private final RecordRdbRepository recordRdbRepository;
 
+    @Override
     @Transactional
     public void execute(final RecordIdValue recordIdValue) {
-        // レコードが存在しない場合
-        if (Objects.isNull(recordRdbRepository.fetchRecord(recordIdValue))) {
-            throw new ValidationException("Record Not Found");
-        }
+        Optional.ofNullable(recordRdbRepository.fetchRecord(recordIdValue))
+                .orElseThrow(() -> new ValidationException("Record Not Found"));
     }
 }
