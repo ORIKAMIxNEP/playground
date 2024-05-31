bedd0b1aeca6aaed64d341d0614e3ce366d80f19
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-31 16:35:34 +0900

    Update AddRecordController.java

--- application/src/main/java/com/spring_boot_template/presentation/controller/record/AddRecordController.java
+++ application/src/main/java/com/spring_boot_template/presentation/controller/record/AddRecordController.java
@@ -1,49 +1,30 @@
 package com.spring_boot_template.presentation.controller.record;
 
-import com.spring_boot_template.application.usecase.record.impl.AddRecordUseCaseImpl;
-import com.spring_boot_template.domain.exception.AccessDeniedException;
+import com.spring_boot_template.application.usecase.record.AddRecordUseCase;
 import com.spring_boot_template.presentation.controller.record.request.AddRecordRequest;
-import com.spring_boot_template.presentation.validator.CsrfTokenValidator;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 final class AddRecordController {
-    private final CsrfTokenValidator csrfTokenValidator;
-    private final AddRecordUseCaseImpl addRecordUseCaseImpl;
+    private final AddRecordUseCase addRecordUseCase;
 
     @PostMapping(value = "/record", consumes = MediaType.APPLICATION_JSON_VALUE)
     @Operation(
             tags = {"record"},
             summary = "レコードを追加する",
             description = "カラム1、カラム2を受け取る" + " → レコードを追加する",
-            responses = {
-                @ApiResponse(responseCode = "204", description = "No Content"),
-                @ApiResponse(
-                        responseCode = "403",
-                        description = "Forbidden",
-                        content =
-                                @Content(
-                                        schema =
-                                                @Schema(
-                                                        implementation =
-                                                                AccessDeniedException.class)))
-            })
-    public ResponseEntity<String> addRecord(
-            @RequestHeader("X-CSRF-Token") String receivedCsrfToken,
-            @RequestBody final AddRecordRequest addRecordRequest) {
-        csrfTokenValidator.execute(receivedCsrfToken);
-        addRecordUseCaseImpl.execute(addRecordRequest);
+            responses = {@ApiResponse(responseCode = "204", description = "No Content")})
+    public ResponseEntity<String> addRecord(@RequestBody final AddRecordRequest addRecordRequest) {
+        addRecordUseCase.execute(addRecordRequest);
+
         return ResponseEntity.noContent().build();
     }
 }
