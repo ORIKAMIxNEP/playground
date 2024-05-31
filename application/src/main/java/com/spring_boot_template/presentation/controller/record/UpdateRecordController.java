d413badff22b1a6ad3840a3de3983fbe3a7fabce
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-31 16:36:39 +0900

    Update UpdateRecordController.java

--- application/src/main/java/com/spring_boot_template/presentation/controller/record/UpdateRecordController.java
+++ application/src/main/java/com/spring_boot_template/presentation/controller/record/UpdateRecordController.java
@@ -1,10 +1,8 @@
 package com.spring_boot_template.presentation.controller.record;
 
-import com.spring_boot_template.application.usecase.record.impl.UpdateRecordUseCaseImpl;
-import com.spring_boot_template.domain.exception.AccessDeniedException;
+import com.spring_boot_template.application.usecase.record.UpdateRecordUseCase;
 import com.spring_boot_template.domain.exception.ValidationException;
 import com.spring_boot_template.presentation.controller.record.request.UpdateRecordRequest;
-import com.spring_boot_template.presentation.validator.CsrfTokenValidator;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -14,15 +12,13 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 final class UpdateRecordController {
-    private final CsrfTokenValidator csrfTokenValidator;
-    private final UpdateRecordUseCaseImpl updateRecordUseCaseImpl;
+    private final UpdateRecordUseCase updateRecordUseCase;
 
     @PutMapping(value = "/record", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
@@ -40,22 +36,12 @@ final class UpdateRecordController {
                                         schema =
                                                 @Schema(
                                                         implementation =
-                                                                ValidationException.class))),
-                @ApiResponse(
-                        responseCode = "403",
-                        description = "Forbidden",
-                        content =
-                                @Content(
-                                        schema =
-                                                @Schema(
-                                                        implementation =
-                                                                AccessDeniedException.class)))
+                                                                ValidationException.class)))
             })
     public ResponseEntity<?> updateRecord(
-            @RequestHeader("X-CSRF-TOKEN") String receivedCsrfToken,
             @RequestBody final UpdateRecordRequest updateRecordRequest) {
-        csrfTokenValidator.execute(receivedCsrfToken);
-        updateRecordUseCaseImpl.execute(updateRecordRequest);
+        updateRecordUseCase.execute(updateRecordRequest);
+
         return ResponseEntity.noContent().build();
     }
 }
