5e8bb35f4bf04d00d225a25002b959ec93c1de9c
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-31 16:32:32 +0900

    Create IdGeneratorTest.java

---
+++ application/src/test/java/com/spring_boot_template/domain/shared/IdGeneratorTest.java
@@ -0,0 +1,17 @@
+package com.spring_boot_template.domain.shared;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public final class IdGeneratorTest {
+    @Test
+    public void generateTest() {
+        final String id = IdGenerator.generate();
+
+        assertThat(id.length()).isEqualTo(26);
+    }
+}
