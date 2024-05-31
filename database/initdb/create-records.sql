270762929a29f54345131dd673c6832aeee18c7d
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-31 16:27:08 +0900

    Update create-records.sql

--- database/initdb/create-records.sql
+++ database/initdb/create-records.sql
@@ -1,11 +1,11 @@
 CREATE TABLE records
 (
-    record_id uuid     NOT NULL,
-    column1   SMALLINT NOT NULL,
-    column2   TEXT     NOT NULL,
+    record_id VARCHAR(26) NOT NULL,
+    column1   SMALLINT    NOT NULL,
+    column2   TEXT        NOT NULL,
     PRIMARY KEY (record_id)
 );
 CREATE INDEX ON records (record_id);
 
 INSERT INTO records (record_id, column1, column2)
-VALUES ('01234567-89ab-cdef-0123-456789abcdef', 0, 'a');
+VALUES ('0123456789ABCDEFGHJKMNPQRS', 0, 'a');
