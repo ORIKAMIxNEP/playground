fa55710ea275820ea49e70970cf8ada9e236e874
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-05-31 16:48:03 +0900

    Update SecurityConfig.java

--- application/src/main/java/com/spring_boot_template/config/SecurityConfig.java
+++ application/src/main/java/com/spring_boot_template/config/SecurityConfig.java
@@ -3,14 +3,17 @@ package com.spring_boot_template.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig {
     @Bean
-    public SecurityFilterChain configHttpSecurity(final HttpSecurity httpSecurity)
+    public SecurityFilterChain securityFilterChain(final HttpSecurity httpSecurity)
             throws Exception {
-        httpSecurity.csrf().disable();
+        httpSecurity.csrf((csrf) -> csrf.ignoringRequestMatchers("/record"));
+
         return httpSecurity.build();
     }
 }
