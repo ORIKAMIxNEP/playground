3e468f7627f52a44d70c64989e660b82d929c64a
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-03 16:34:49 +0900

    Update UpdateTaskRequest.java

--- application/src/main/java/com/spring_boot_template/presentation/controller/task/request/UpdateTaskRequest.java
+++ application/src/main/java/com/spring_boot_template/presentation/controller/task/request/UpdateTaskRequest.java
@@ -3,22 +3,22 @@ package com.spring_boot_template.presentation.controller.task.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(
-        description = "更新するレコードのデータ",
-        requiredProperties = {"recordId", "column1", "column2"})
+        description = "更新するタスクのデータ",
+        requiredProperties = {"taskId", "column1", "column2"})
 public record UpdateTaskRequest(
         @Schema(
-                        title = "レコードID",
+                        title = "タスクID",
                         type = "string",
                         format = "uuid",
                         example = "01234567-89ab-cdef-0123-456789abcdef")
-                String recordId,
+                String taskId,
         @Schema(
-                        title = "レコードカラム1",
+                        title = "タスクカラム1",
                         type = "integer",
                         format = "int32",
                         maximum = "127",
                         minimum = "0",
                         example = "0")
                 Byte column1,
-        @Schema(title = "レコードカラム2", type = "string", maxLength = 10, example = "a")
+        @Schema(title = "タスクカラム2", type = "string", maxLength = 10, example = "a")
                 String column2) {}
