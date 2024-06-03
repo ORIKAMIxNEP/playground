bd7d11d6b6f5552556d68021182c771198852909
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-03 16:27:17 +0900

    Update FetchTaskByTaskIdUseCaseImpl.java

--- application/src/main/java/com/spring_boot_template/application/usecase/task/impl/FetchTaskByTaskIdUseCaseImpl.java
+++ application/src/main/java/com/spring_boot_template/application/usecase/task/impl/FetchTaskByTaskIdUseCaseImpl.java
@@ -1,7 +1,7 @@
 package com.spring_boot_template.application.usecase.task.impl;
 
 import com.spring_boot_template.application.usecase.task.FetchTaskByTaskIdUseCase;
-import com.spring_boot_template.domain.exception.LogicValidationException;
+import com.spring_boot_template.domain.exception.ValidationException;
 import com.spring_boot_template.domain.model.task.TaskEntity;
 import com.spring_boot_template.domain.model.task.TaskRepository;
 import com.spring_boot_template.domain.model.task.value.AssignedUserIdValue;
@@ -16,16 +16,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class FetchTaskByTaskIdUseCaseImpl implements FetchTaskByTaskIdUseCase {
-    private final TaskRepository recordRepository;
+    private final TaskRepository taskRepository;
 
     @Override
     @Transactional
-    public FetchTaskResponse execute(final String recordId) {
-        final TaskIdValue taskIdValue = new TaskIdValue(recordId);
+    public FetchTaskResponse execute(final String taskId) {
+        final TaskIdValue taskIdValue = new TaskIdValue(taskId);
 
         final TaskEntity taskEntity =
-                Optional.ofNullable(recordRepository.fetchTaskByTaskId(taskIdValue))
-                        .orElseThrow(() -> new LogicValidationException("Record is not found"));
+                Optional.ofNullable(taskRepository.fetchTaskByTaskId(taskIdValue))
+                        .orElseThrow(() -> new ValidationException("Task is not found"));
 
         final AssignedUserIdValue assignedUserIdValue = taskEntity.getAssignedUserIdValue();
         final byte column1 = assignedUserIdValue.getValue();
