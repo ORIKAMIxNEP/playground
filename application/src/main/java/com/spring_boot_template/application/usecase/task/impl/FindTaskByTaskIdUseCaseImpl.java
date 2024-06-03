f98f925d31db84733356587480738256e2c9c23b
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-03 16:27:32 +0900

    Update FindTaskByTaskIdUseCaseImpl.java

--- application/src/main/java/com/spring_boot_template/application/usecase/task/impl/FindTaskByTaskIdUseCaseImpl.java
+++ application/src/main/java/com/spring_boot_template/application/usecase/task/impl/FindTaskByTaskIdUseCaseImpl.java
@@ -1,7 +1,7 @@
 package com.spring_boot_template.application.usecase.task.impl;
 
 import com.spring_boot_template.application.usecase.task.FindTaskByTaskIdUseCase;
-import com.spring_boot_template.domain.exception.LogicValidationException;
+import com.spring_boot_template.domain.exception.ValidationException;
 import com.spring_boot_template.domain.model.task.TaskEntity;
 import com.spring_boot_template.domain.model.task.TaskRepository;
 import com.spring_boot_template.domain.model.task.value.TaskIdValue;
@@ -13,12 +13,12 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 class FindTaskByTaskIdUseCaseImpl implements FindTaskByTaskIdUseCase {
-    private final TaskRepository recordRepository;
+    private final TaskRepository taskRepository;
 
     @Override
     @Transactional
     public TaskEntity execute(final TaskIdValue taskId) {
-        return Optional.ofNullable(recordRepository.fetchTaskByTaskId(taskId))
-                .orElseThrow(() -> new LogicValidationException("Error: Task is not found"));
+        return Optional.ofNullable(taskRepository.fetchTaskByTaskId(taskId))
+                .orElseThrow(() -> new ValidationException("Error: Task is not found"));
     }
 }
