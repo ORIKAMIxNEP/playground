cf7d2a93ffae90bfd6adfbd1a9a395c54a925afa
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-03 16:31:29 +0900

    Update TaskEntity.java

--- application/src/main/java/com/spring_boot_template/domain/model/task/TaskEntity.java
+++ application/src/main/java/com/spring_boot_template/domain/model/task/TaskEntity.java
@@ -1,5 +1,6 @@
 package com.spring_boot_template.domain.model.task;
 
+import com.spring_boot_template.domain.exception.DomainException;
 import com.spring_boot_template.domain.model.task.value.DueDateValue;
 import com.spring_boot_template.domain.model.task.value.MaxPostponeCountValue;
 import com.spring_boot_template.domain.model.task.value.PostponeCountValue;
@@ -22,4 +23,13 @@ public class TaskEntity {
     private DueDateValue dueDate;
     private PostponeCountValue postponeCount;
     private MaxPostponeCountValue MaxPostponeCount;
+
+    public final void updateTaskStatus(final TaskStatusValue updatedTaskStatus) {
+        // 更新されたタスクステータスと期待されたタスクステータスが異なる場合
+        if (!updatedTaskStatus.equals(taskStatus.nextTaskStatus())) {
+            throw new DomainException("UpdatedTaskStatus does not match expected task status");
+        }
+
+        taskStatus = updatedTaskStatus;
+    }
 }
