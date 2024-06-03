c9bd1093e8eaf350166205c7c7519412149cbde6
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-03 16:35:07 +0900

    Update FetchTaskResponse.java

--- application/src/main/java/com/spring_boot_template/presentation/controller/task/response/FetchTaskResponse.java
+++ application/src/main/java/com/spring_boot_template/presentation/controller/task/response/FetchTaskResponse.java
@@ -2,10 +2,10 @@ package com.spring_boot_template.presentation.controller.task.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "取得したレコードのデータ")
+@Schema(description = "取得したタスクのデータ")
 public record FetchTaskResponse(
         @Schema(
-                        title = "レコードカラム1",
+                        title = "タスクカラム1",
                         type = "integer",
                         format = "int32",
                         maximum = "127",
@@ -14,7 +14,7 @@ public record FetchTaskResponse(
                         example = "0")
                 Byte column1,
         @Schema(
-                        title = "レコードカラム2",
+                        title = "タスクカラム2",
                         type = "string",
                         maxLength = 10,
                         minLength = 1,
