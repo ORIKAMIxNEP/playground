6457b7bf755ea862f469907af665faa96a8a8b83
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-14 14:59:43 +0900

    Create TaskDto.java

---
+++ application/src/main/java/com/spring_boot_template/infrastructure/project/dto/TaskDto.java
@@ -0,0 +1,14 @@
+package com.spring_boot_template.infrastructure.project.request;
+
+import java.util.List;
+
+public record TaskDto (
+  TaskId id,
+  TaskName name,
+  Status status,
+  List<AccountId> assignedAccountIds,
+  DueDate dueDate,
+  PostponeCount postponeCount,
+  MaxPostponeCount maxPostponeCount,
+  int index
+) {}
