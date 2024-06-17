23a3078b3904306afb76c5f974f88d21798b8d26
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-17 16:02:27 +0900

    Update and rename Date.java to DueDate.java

--- application/src/main/java/com/spring_boot_template/domain/model/project/task/due_date_detail/value/DueDate.java
+++ application/src/main/java/com/spring_boot_template/domain/model/project/task/due_date_detail/value/DueDate.java
@@ -1,9 +1,9 @@
-package com.spring_boot_template.domain.model.project.task.due_date.value;
+package com.spring_boot_template.domain.model.project.task.due_date_detail.value;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public final class Date {
+public final class DueDate {
     @Getter private final String value;
 }
