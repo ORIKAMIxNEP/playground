da7c498fe32dd79ce3151c0d5ca17480bc4898f1
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-17 15:58:43 +0900

    Update ProjectDto.java

--- application/src/main/java/com/spring_boot_template/infrastructure/project/dto/ProjectDto.java
+++ application/src/main/java/com/spring_boot_template/infrastructure/project/dto/ProjectDto.java
@@ -1,12 +1,7 @@
 package com.spring_boot_template.infrastructure.project.dto;
 
 import com.spring_boot_template.domain.model.account.value.AccountId;
-import com.spring_boot_template.domain.model.project.value.ProjectId;
 import com.spring_boot_template.domain.model.project.value.ProjectName;
-import java.util.List;
+import java.util.HashSet;
 
-public record ProjectDto(
-        ProjectId id,
-        ProjectName name,
-        List<AccountId> participatingAccountIds,
-        List<TaskDto> tasks) {}
+public record ProjectDto(ProjectName name, HashSet<AccountId> participatingAccountIds) {}
