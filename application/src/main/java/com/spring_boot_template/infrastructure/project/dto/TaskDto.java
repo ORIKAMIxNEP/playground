8b5efa819d6d067eaa6861f866fae777a5613611
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-17 15:58:24 +0900

    Update TaskDto.java

--- application/src/main/java/com/spring_boot_template/infrastructure/project/dto/TaskDto.java
+++ application/src/main/java/com/spring_boot_template/infrastructure/project/dto/TaskDto.java
@@ -1,20 +1,14 @@
 package com.spring_boot_template.infrastructure.project.dto;
 
 import com.spring_boot_template.domain.model.account.value.AccountId;
-import com.spring_boot_template.domain.model.project.task.due_date.value.Date;
-import com.spring_boot_template.domain.model.project.task.due_date.value.MaxPostponeCount;
-import com.spring_boot_template.domain.model.project.task.due_date.value.PostponeCount;
 import com.spring_boot_template.domain.model.project.task.value.Status;
 import com.spring_boot_template.domain.model.project.task.value.TaskId;
 import com.spring_boot_template.domain.model.project.task.value.TaskName;
-import java.util.List;
+import java.util.HashSet;
 
 public record TaskDto(
         TaskId id,
         TaskName name,
         Status status,
-        List<AccountId> assignedAccountIds,
-        Date date,
-        PostponeCount postponeCount,
-        MaxPostponeCount maxPostponeCount,
+        HashSet<AccountId> assignedAccountIds,
         int index) {}
