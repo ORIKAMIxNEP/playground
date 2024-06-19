3f2c1eb658774e5049eda3d3bbbdc6bc6a4f4c79
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-19 16:11:36 +0900

    Update TaskDto.java

--- application/src/main/java/com/spring_boot_template/infrastructure/project/dto/TaskDto.java
+++ application/src/main/java/com/spring_boot_template/infrastructure/project/dto/TaskDto.java
@@ -4,11 +4,17 @@ import com.spring_boot_template.domain.model.account.value.AccountId;
 import com.spring_boot_template.domain.model.project.task.value.Status;
 import com.spring_boot_template.domain.model.project.task.value.TaskId;
 import com.spring_boot_template.domain.model.project.task.value.TaskName;
-import java.util.HashSet;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public record TaskDto(
-        TaskId id,
-        TaskName name,
-        Status status,
-        HashSet<AccountId> assignedAccountIds,
-        int index) {}
+import java.util.ArrayList;
+
+@NoArgsConstructor(force = true)
+@Getter
+public class TaskDto {
+    private final TaskId taskId;
+    private final TaskName taskName;
+    private final Status status;
+    private final ArrayList<AccountId> assignedAccountIds;
+    private final int index;
+}
