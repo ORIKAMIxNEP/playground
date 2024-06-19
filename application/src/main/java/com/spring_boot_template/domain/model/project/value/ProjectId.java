1545508666d90f8983ed8534de71363576bdd4a4
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-19 16:09:55 +0900

    Update ProjectId.java

--- application/src/main/java/com/spring_boot_template/domain/model/project/value/ProjectId.java
+++ application/src/main/java/com/spring_boot_template/domain/model/project/value/ProjectId.java
@@ -2,12 +2,10 @@ package com.spring_boot_template.domain.model.project.value;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@EqualsAndHashCode
 public final class ProjectId {
     @Getter
     @NotNull(message = "ProjectId is null")
