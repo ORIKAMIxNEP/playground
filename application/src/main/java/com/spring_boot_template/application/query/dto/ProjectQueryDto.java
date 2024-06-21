72c6f18bbbc5a9ae4520363f9ae65dcaf23b2366
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-21 17:16:28 +0900

    Create ProjectQueryDto.java

---
+++ application/src/main/java/com/spring_boot_template/application/query/dto/ProjectQueryDto.java
@@ -0,0 +1,6 @@
+package com.spring_boot_template.application.query.dto;
+
+import com.spring_boot_template.domain.model.project.value.ProjectId;
+import com.spring_boot_template.domain.model.project.value.ProjectName;
+
+public record ProjectQueryDto(ProjectId projectId, ProjectName projectName) {}
