0cec971c1a1e2f3a0b93fec04c01584119724a18
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-21 17:19:38 +0900

    Create ExistsProjectDomainServiceImpl.java

---
+++ application/src/main/java/com/spring_boot_template/domain/model/project/service/impl/ExistsProjectDomainServiceImpl.java
@@ -0,0 +1,23 @@
+package com.spring_boot_template.domain.model.project.service.impl;
+
+import com.spring_boot_template.application.query.ProjectQueryService;
+import com.spring_boot_template.domain.exception.ValidationException;
+import com.spring_boot_template.domain.model.account.value.AccountId;
+import com.spring_boot_template.domain.model.project.service.ExistsProjectDomainService;
+import com.spring_boot_template.domain.model.project.value.ProjectId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+class ExistsProjectDomainServiceImpl implements ExistsProjectDomainService {
+    private final ProjectQueryService projectQueryService;
+
+    @Override
+    public void execute(final AccountId accountId, final ProjectId projectId) {
+        projectQueryService.fetchProjectIdsByAccountId(accountId).stream()
+                .filter(projectId::equals)
+                .findFirst()
+                .orElseThrow(() -> new ValidationException("Project is not found"));
+    }
+}
