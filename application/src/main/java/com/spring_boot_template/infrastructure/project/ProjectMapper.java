b10ebc93b60b6c1a3bc1ecfa3b4301c85b1af8a1
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-21 17:21:16 +0900

    Update ProjectMapper.java

--- application/src/main/java/com/spring_boot_template/infrastructure/project/ProjectMapper.java
+++ application/src/main/java/com/spring_boot_template/infrastructure/project/ProjectMapper.java
@@ -3,24 +3,27 @@ package com.spring_boot_template.infrastructure.project;
 import com.spring_boot_template.domain.model.account.value.AccountId;
 import com.spring_boot_template.domain.model.project.Project;
 import com.spring_boot_template.domain.model.project.task.Task;
+import com.spring_boot_template.domain.model.project.task.due_date_detail.DueDateDetail;
 import com.spring_boot_template.domain.model.project.task.value.TaskId;
 import com.spring_boot_template.domain.model.project.value.ProjectId;
 import com.spring_boot_template.infrastructure.project.dto.DueDateDetailDto;
 import com.spring_boot_template.infrastructure.project.dto.ProjectDto;
 import com.spring_boot_template.infrastructure.project.dto.TaskDto;
 import java.util.ArrayList;
-import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 interface ProjectMapper {
-    void insertProject(final Project project, final List<AccountId> participatingAccountIds);
+    void upsertProject(final Project project);
 
-    void insertTask(
-            final ProjectId projectId,
-            final Task task,
-            final List<AccountId> assignedAccountIds,
-            final int index);
+    void insertProjectParticipatingAccount(
+            final ProjectId projectId, final AccountId participatingAccountId);
+
+    void insertTask(final ProjectId projectId, final Task task, final int index);
+
+    void insertTaskAssignedAccount(final TaskId taskId, final AccountId assignedAccountId);
+
+    void insertDueDateDetail(final TaskId taskId, final DueDateDetail dueDateDetail);
 
     ProjectDto selectProjectByProjectId(final ProjectId projectId);
 
@@ -28,11 +31,9 @@ interface ProjectMapper {
 
     ArrayList<DueDateDetailDto> selectDueDateDetailsByProjectId(final ProjectId projectId);
 
-    void updateProject(final Project project);
-
-    void updateTask(final Task task);
-
     void deleteProject(final ProjectId projectId);
 
-    void deleteTask(final TaskId taskId);
+    void deleteProjectParticipatingAccount(final ProjectId projectId);
+
+    void deleteTask(final ProjectId projectId);
 }
