f898fdc10002448443f5fd9eaa4f363d5cb5972b
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-26 14:45:43 +0900

    Update compose.sh

--- compose.sh
+++ compose.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-COMPOSE_FILE_PATH="docker-compose/compose.yml"
-
 if [ $# -eq 0 ]; then
-    docker compose -f COMPOSE_FILE_PATH build --no-cache
-    docker compose -f COMPOSE_FILE_PATH up -d
+    docker compose build --no-cache
+    docker compose up -d
     echo "Database IPAddress: $(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' database)"
 else
     while getopts "de:l:" option; do
         case $option in
             d)
-                docker compose -f COMPOSE_FILE_PATH down -v
+                docker compose down -v
                 ;;
             e)
                 if [ -n "$OPTARG" ]; then
-                    docker compose -f COMPOSE_FILE_PATH exec "$OPTARG" bash
+                    docker compose exec "$OPTARG" bash
                 fi
                 ;;
             l)
                 if [ -n "$OPTARG" ]; then
-                    docker compose -f COMPOSE_FILE_PATH logs "$OPTARG"
+                    docker compose logs "$OPTARG"
                 fi
                 ;;
             \?)
