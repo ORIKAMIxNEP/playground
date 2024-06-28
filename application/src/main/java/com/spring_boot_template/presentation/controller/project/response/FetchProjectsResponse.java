70838894c1be1fef757c5c5d6bf6c2a45973e132
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-28 11:27:18 +0900

    Update FetchProjectsResponse.java

--- application/src/main/java/com/spring_boot_template/presentation/controller/project/response/FetchProjectsResponse.java
+++ application/src/main/java/com/spring_boot_template/presentation/controller/project/response/FetchProjectsResponse.java
@@ -3,6 +3,6 @@ package com.spring_boot_template.presentation.controller.project.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
-@Schema(description = "取得したプロジェクトリスト")
+@Schema(title = "取得したプロジェクトリスト")
 public record FetchProjectsResponse(
         @Schema(title = "プロジェクトリスト", type = "array") List<ProjectResponse> projects) {}
