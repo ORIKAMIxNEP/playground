974cdde234ebf55666d1f554b3a318a63faba73d
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-28 11:41:04 +0900

    Update ProjectResponse.java

--- application/src/main/java/com/spring_boot_template/presentation/controller/project/response/ProjectResponse.java
+++ application/src/main/java/com/spring_boot_template/presentation/controller/project/response/ProjectResponse.java
@@ -2,7 +2,7 @@ package com.spring_boot_template.presentation.controller.project.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "プロジェクト")
+@Schema(title = "プロジェクト")
 public record ProjectResponse(
         @Schema(title = "プロジェクトID", type = "string", example = "1123456789ABCDEFGHJKMNPQRS")
                 String projectId,
