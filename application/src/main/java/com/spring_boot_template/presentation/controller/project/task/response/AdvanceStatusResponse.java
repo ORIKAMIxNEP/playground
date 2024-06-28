d28de8e9c54cc9270611f00b583fb487e38bf5e5
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-28 11:41:44 +0900

    Update AdvanceStatusResponse.java

--- application/src/main/java/com/spring_boot_template/presentation/controller/project/task/response/AdvanceStatusResponse.java
+++ application/src/main/java/com/spring_boot_template/presentation/controller/project/task/response/AdvanceStatusResponse.java
@@ -2,6 +2,6 @@ package com.spring_boot_template.presentation.controller.project.task.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "進めたステータス")
+@Schema(title = "進めたステータス")
 public record AdvanceStatusResponse(
         @Schema(title = "ステータス", type = "string", example = "DOING") String status) {}
