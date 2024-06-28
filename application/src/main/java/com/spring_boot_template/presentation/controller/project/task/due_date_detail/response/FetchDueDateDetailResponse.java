dd1d3246aeeeb22b591af57874236f762468d056
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-28 11:42:34 +0900

    Update FetchDueDateDetailResponse.java

--- application/src/main/java/com/spring_boot_template/presentation/controller/project/task/due_date_detail/response/FetchDueDateDetailResponse.java
+++ application/src/main/java/com/spring_boot_template/presentation/controller/project/task/due_date_detail/response/FetchDueDateDetailResponse.java
@@ -2,9 +2,14 @@ package com.spring_boot_template.presentation.controller.project.task.due_date_d
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "取得した詳細締め切り期日")
+@Schema(title = "取得した詳細締め切り期日")
 public record FetchDueDateDetailResponse(
-        @Schema(title = "締め切り期日", example = "Date") String dueDate,
+        @Schema(
+                        title = "締め切り期日",
+                        type = "string",
+                        format = "date-time",
+                        example = "2000-01-01T00:00:00Z")
+                String dueDate,
         @Schema(title = "延期回数", type = "integer", format = "int32", example = "0")
                 int postponeCount,
         @Schema(title = "最大延期回数", type = "integer", format = "int32", example = "10")
