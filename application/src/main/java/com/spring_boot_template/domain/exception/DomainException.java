fc67d8f89365792a20a763a14b2e425c4129970a
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-28 11:26:10 +0900

    Update DomainException.java

--- application/src/main/java/com/spring_boot_template/domain/exception/DomainException.java
+++ application/src/main/java/com/spring_boot_template/domain/exception/DomainException.java
@@ -1,5 +1,8 @@
 package com.spring_boot_template.domain.exception;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(title = "ドメイン知識例外")
 public final class DomainException extends RuntimeException {
     public DomainException(final String message) {
         super(message);
