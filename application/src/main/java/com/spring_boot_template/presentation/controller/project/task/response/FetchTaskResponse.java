352003ab02f49b8802e27b0d22fed8229259a12c
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-28 11:41:59 +0900

    Update FetchTaskResponse.java

--- application/src/main/java/com/spring_boot_template/presentation/controller/project/task/response/FetchTaskResponse.java
+++ application/src/main/java/com/spring_boot_template/presentation/controller/project/task/response/FetchTaskResponse.java
@@ -3,10 +3,10 @@ package com.spring_boot_template.presentation.controller.project.task.response;
 import com.spring_boot_template.presentation.controller.project.task.due_date_detail.response.FetchDueDateDetailResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "取得したタスク")
+@Schema(title = "取得したタスク")
 public record FetchTaskResponse(
         @Schema(title = "タスク名", type = "string", example = "TaskName") String taskName,
         @Schema(title = "ステータス", type = "string", example = "UNDONE") String status,
         @Schema(title = "担当アカウントID", type = "array", example = "[0123456789ABCDEFGHJKMNPQRS]")
                 String[] assignedAccountId,
-        @Schema(title = "詳細締め切り期日", type = "object") FetchDueDateDetailResponse dueDateDetail) {}
+        FetchDueDateDetailResponse fetchDueDateDetailResponse) {}
