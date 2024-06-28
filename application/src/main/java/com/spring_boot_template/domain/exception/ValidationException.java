bfd0a51ba4e814171286117cd969a2a894ef5aaa
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2024-06-28 11:26:24 +0900

    Update ValidationException.java

--- application/src/main/java/com/spring_boot_template/domain/exception/ValidationException.java
+++ application/src/main/java/com/spring_boot_template/domain/exception/ValidationException.java
@@ -1,5 +1,8 @@
 package com.spring_boot_template.domain.exception;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(title = "バリデーション例外")
 public final class ValidationException extends RuntimeException {
     public ValidationException(final String message) {
         super(message);
