145cd10dd0ef5ab7f06fbb559c06214522076484
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-16 17:28:03 +0900

    Update and rename ProjectResponses.java to FetchProjectsResponse.java

--- application/src/main/java/com/spring_boot_template/presentation/controller/project/response/FetchProjectsResponse.java
+++ application/src/main/java/com/spring_boot_template/presentation/controller/project/response/FetchProjectsResponse.java
@@ -4,5 +4,5 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
 @Schema(title = "取得したプロジェクトリスト")
-public record ProjectResponses(
+public record FetchProjectsResponse(
         @Schema(title = "プロジェクトリスト", type = "array") List<ProjectResponse> projects) {}
