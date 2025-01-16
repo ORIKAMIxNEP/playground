a0160d28557cb7b2853fe138d35bcb5ffb2ad3d8
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-16 17:27:28 +0900

    Update FetchProjectsController.java

--- application/src/main/java/com/spring_boot_template/presentation/controller/project/FetchProjectsController.java
+++ application/src/main/java/com/spring_boot_template/presentation/controller/project/FetchProjectsController.java
@@ -1,15 +1,12 @@
 package com.spring_boot_template.presentation.controller.project;
 
 import com.spring_boot_template.application.project.FetchProjectsUseCase;
-import com.spring_boot_template.application.project.query.ProjectQueryDto;
 import com.spring_boot_template.domain.model.account.value.AccountId;
-import com.spring_boot_template.presentation.controller.project.converter.ProjectResponsesConverter;
-import com.spring_boot_template.presentation.controller.project.response.ProjectResponses;
+import com.spring_boot_template.presentation.controller.project.response.FetchProjectsResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 final class FetchProjectsController {
     private final FetchProjectsUseCase fetchProjectsUseCase;
-    private final ProjectResponsesConverter projectResponsesConverter;
 
     @GetMapping(value = "/projects")
     @ResponseBody
@@ -34,13 +30,14 @@ final class FetchProjectsController {
                         content =
                                 @Content(
                                         mediaType = "application/json",
-                                        schema = @Schema(implementation = ProjectResponses.class)))
+                                        schema =
+                                                @Schema(
+                                                        implementation =
+                                                                FetchProjectsResponse.class)))
             })
     private ResponseEntity<?> execute() {
-        final AccountId participatingAccountId = new AccountId("0123456789ABCDEFGHJKMNPQRS");
-        final List<ProjectQueryDto> projectQueryDtos =
-                fetchProjectsUseCase.execute(participatingAccountId);
+        final AccountId accountId = new AccountId("0123456789ABCDEFGHJKMNPQRS");
 
-        return ResponseEntity.ok(projectResponsesConverter.execute(projectQueryDtos));
+        return ResponseEntity.ok(fetchProjectsUseCase.execute(accountId));
     }
 }
