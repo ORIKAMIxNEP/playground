fc3d40564346de6a89db31a22dc576a782f49e6b
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-16 17:33:25 +0900

    Update FetchTaskUseCaseImpl.java

--- application/src/main/java/com/spring_boot_template/application/task/impl/FetchTaskUseCaseImpl.java
+++ application/src/main/java/com/spring_boot_template/application/task/impl/FetchTaskUseCaseImpl.java
@@ -1,14 +1,10 @@
 package com.spring_boot_template.application.task.impl;
 
-import com.spring_boot_template.application.due_date_detail.query.DueDateDetailQueryDto;
-import com.spring_boot_template.application.due_date_detail.query.DueDateDetailQueryService;
 import com.spring_boot_template.application.task.FetchTaskUseCase;
-import com.spring_boot_template.application.task.query.TaskQueryDto;
 import com.spring_boot_template.application.task.query.TaskQueryService;
-import com.spring_boot_template.domain.exception.ResourceNotFoundException;
 import com.spring_boot_template.domain.model.project.value.ProjectId;
 import com.spring_boot_template.domain.model.task.value.TaskId;
-import java.util.Optional;
+import com.spring_boot_template.presentation.controller.task.response.FetchTaskResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,25 +13,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 class FetchTaskUseCaseImpl implements FetchTaskUseCase {
     private final TaskQueryService taskQueryService;
-    private final DueDateDetailQueryService dueDateDetailQueryService;
 
     @Override
     @Transactional
-    public TaskQueryDto execute(final String projectIdRequest, final String taskIdRequest) {
+    public FetchTaskResponse execute(final String projectIdRequest, final String taskIdRequest) {
         final ProjectId projectId = new ProjectId(projectIdRequest);
         final TaskId taskId = new TaskId(taskIdRequest);
 
-        final Optional<TaskQueryDto> optionalTaskDto =
-                taskQueryService.findTaskByProjectIdAndTaskId(projectId, taskId);
-        final TaskQueryDto taskQueryDto =
-                optionalTaskDto.orElseThrow(
-                        () -> new ResourceNotFoundException("Task is not found"));
-
-        final Optional<DueDateDetailQueryDto> optionalDueDateDetailDto =
-                dueDateDetailQueryService.findDueDateDetailByTaskId(taskId);
-        final DueDateDetailQueryDto dueDateDetailQueryDto = optionalDueDateDetailDto.orElse(null);
-        taskQueryDto.setDueDateDetailQueryDto(dueDateDetailQueryDto);
-
-        return taskQueryDto;
+        return taskQueryService.findTaskByProjectIdAndTaskId(projectId, taskId);
     }
 }
