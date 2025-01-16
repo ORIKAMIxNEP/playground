4f92ba0af8aaad7e351adfc87f6c2f326af9f1c4
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-16 17:41:54 +0900

    Update ProjectQueryServiceImpl.java

--- application/src/main/java/com/spring_boot_template/infrastructure/project/query/ProjectQueryServiceImpl.java
+++ application/src/main/java/com/spring_boot_template/infrastructure/project/query/ProjectQueryServiceImpl.java
@@ -3,6 +3,8 @@ package com.spring_boot_template.infrastructure.project.query;
 import com.spring_boot_template.application.project.query.ProjectQueryDto;
 import com.spring_boot_template.application.project.query.ProjectQueryService;
 import com.spring_boot_template.domain.model.account.value.AccountId;
+import com.spring_boot_template.presentation.controller.project.response.FetchProjectsResponse;
+import com.spring_boot_template.presentation.controller.project.response.ProjectResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,8 +15,21 @@ final class ProjectQueryServiceImpl implements ProjectQueryService {
     private final ProjectQueryMapper projectQueryMapper;
 
     @Override
-    public List<ProjectQueryDto> findProjectsByParticipatingAccountId(
-            final AccountId participatingAccountId) {
-        return projectQueryMapper.selectProjectsByParticipatingAccountId(participatingAccountId);
+    public FetchProjectsResponse findProjectsByAccountId(final AccountId accountId) {
+        final List<ProjectQueryDto> projectQueryDtos =
+                projectQueryMapper.selectProjectsByAccountId(accountId);
+
+        final List<ProjectResponse> projectResponses =
+                projectQueryDtos.stream()
+                        .map(
+                                projectQueryDto -> {
+                                    final String projectId = projectQueryDto.getProjectId();
+                                    final String projectName = projectQueryDto.getProjectName();
+
+                                    return new ProjectResponse(projectId, projectName);
+                                })
+                        .toList();
+
+        return new FetchProjectsResponse(projectResponses);
     }
 }
