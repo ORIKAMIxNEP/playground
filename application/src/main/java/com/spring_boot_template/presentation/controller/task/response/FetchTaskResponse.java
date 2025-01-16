797921544a6e18e6e95accf572a38b11ece3d449
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-16 17:28:56 +0900

    Update and rename TaskResponse.java to FetchTaskResponse.java

--- application/src/main/java/com/spring_boot_template/presentation/controller/task/response/FetchTaskResponse.java
+++ application/src/main/java/com/spring_boot_template/presentation/controller/task/response/FetchTaskResponse.java
@@ -4,9 +4,9 @@ import com.spring_boot_template.presentation.controller.due_date_detail.response
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(title = "取得したタスク")
-public record TaskResponse(
+public record FetchTaskResponse(
         @Schema(title = "タスク名", type = "string", example = "TaskName") String taskName,
         @Schema(title = "ステータス", type = "string", example = "UNDONE") String status,
         @Schema(title = "担当アカウントID", type = "array", example = "[0123456789ABCDEFGHJKMNPQRS]")
-                String[] assignedAccountId,
+                String[] accountId,
         DueDateDetailResponse dueDateDetailResponse) {}
