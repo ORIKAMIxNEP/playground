678c1eee11a594e41257798524653f36e738271a
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-17 18:18:29 +0900

    Update ProjectQueryServiceImpl.java

--- application/src/main/java/com/spring_boot_template/infrastructure/project/query/ProjectQueryServiceImpl.java
+++ application/src/main/java/com/spring_boot_template/infrastructure/project/query/ProjectQueryServiceImpl.java
@@ -1,5 +1,8 @@
 package com.spring_boot_template.infrastructure.project.query;
 
+import static com.spring_boot_template.jooq.tables.ProjectAccountParticipations.PROJECT_ACCOUNT_PARTICIPATIONS;
+import static com.spring_boot_template.jooq.tables.Projects.PROJECTS;
+
 import com.spring_boot_template.application.project.query.ProjectQueryDto;
 import com.spring_boot_template.application.project.query.ProjectQueryService;
 import com.spring_boot_template.domain.model.account.value.AccountId;
@@ -7,15 +10,40 @@ import com.spring_boot_template.presentation.controller.project.response.FetchPr
 import com.spring_boot_template.presentation.controller.project.response.ProjectResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.jooq.DSLContext;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 final class ProjectQueryServiceImpl implements ProjectQueryService {
-    private final ProjectQueryMapper projectQueryMapper;
+    private final DSLContext dslContext;
 
     @Override
     public FetchProjectsResponse findProjectsByAccountId(final AccountId accountId) {
+        List<ProjectResponse> projectResponses =
+                dslContext
+                        .select(PROJECTS.PROJECT_ID, PROJECTS.PROJECT_NAME)
+                        .from(PROJECTS)
+                        .join(PROJECT_ACCOUNT_PARTICIPATIONS)
+                        .on(PROJECTS.PROJECT_ID.eq(PROJECT_ACCOUNT_PARTICIPATIONS.PROJECT_ID))
+                        .where(PROJECT_ACCOUNT_PARTICIPATIONS.ACCOUNT_ID.eq(accountId.getValue()))
+                        .fetchInto(ProjectResponse.class);
+        //                        .fetch()
+        //                        .stream()
+        //                        .map(
+        //                                record ->
+        //                                        new ProjectResponse(
+        //                                                record.get(PROJECTS.PROJECT_ID),
+        //                                                record.get(PROJECTS.PROJECT_NAME)))
+        //                        .toList();
+
+        return new FetchProjectsResponse(projectResponses);
+    }
+
+    private final ProjectQueryMapper projectQueryMapper;
+
+    // @Override
+    public FetchProjectsResponse findProjectsByAccountId2(final AccountId accountId) {
         final List<ProjectQueryDto> projectQueryDtos =
                 projectQueryMapper.selectProjectsByAccountId(accountId);
 
