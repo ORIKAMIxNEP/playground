9a2ac92e3343d0f5fe3cb3a95521374226b8e687
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-22 15:47:17 +0900

    Update and rename ProjectResponse.java to FetchProjectsResponseProjectElement.java

--- application/src/main/java/com/spring_boot_template/presentation/controller/project/response/FetchProjectsResponseProjectElement.java
+++ application/src/main/java/com/spring_boot_template/presentation/controller/project/response/FetchProjectsResponseProjectElement.java
@@ -3,7 +3,7 @@ package com.spring_boot_template.presentation.controller.project.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(title = "プロジェクト")
-public record ProjectResponse(
+public record FetchProjectsResponseProjectElement(
         @Schema(title = "プロジェクトID", type = "string", example = "1123456789ABCDEFGHJKMNPQRS")
                 String projectId,
         @Schema(title = "プロジェクト名", type = "string", example = "ProjectName") String projectName) {}
