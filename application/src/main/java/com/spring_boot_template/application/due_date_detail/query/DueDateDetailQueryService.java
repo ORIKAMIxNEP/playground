ba68b57d21015be72e6f14743d1fbc4dbbbb1732
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-23 17:34:34 +0900

    Update and rename application/src/main/java/com/spring_boot_template/application/due_date_detail/query/DueDateDetailQueryService.java to application/src/main/java/com/spring_boot_template/application/due_date_detail/.gitkeep

--- application/src/main/java/com/spring_boot_template/application/due_date_detail/query/DueDateDetailQueryService.java
+++
@@ -1,8 +0,0 @@
-package com.spring_boot_template.application.due_date_detail.query;
-
-import com.spring_boot_template.domain.model.task.value.TaskId;
-import java.util.Optional;
-
-public interface DueDateDetailQueryService {
-    Optional<DueDateDetailQueryDto> findDueDateDetailByTaskId(final TaskId taskId);
-}
