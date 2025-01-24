18718fc5b0b09731bb34504d4f7b53fa47519f8c
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-24 17:32:15 +0900

    Update TaskDto.java

--- application/src/main/java/com/spring_boot_template/infrastructure/task/TaskDto.java
+++ application/src/main/java/com/spring_boot_template/infrastructure/task/TaskDto.java
@@ -1,8 +1,7 @@
 package com.spring_boot_template.infrastructure.task;
 
-import com.spring_boot_template.domain.model.account.value.AccountId;
 import com.spring_boot_template.domain.model.task.value.Status;
 import com.spring_boot_template.domain.model.task.value.TaskId;
 import com.spring_boot_template.domain.model.task.value.TaskName;
 
-public record TaskDto(TaskId taskId, TaskName taskName, Status status, AccountId accountId) {}
+public record TaskDto(TaskId taskId, TaskName taskName, Status status) {}
