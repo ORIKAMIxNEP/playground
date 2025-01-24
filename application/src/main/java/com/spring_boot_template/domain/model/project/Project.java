b127df15a2d39473bcc15745cbcc866d76d76f1d
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-24 17:30:30 +0900

    Update Project.java

--- application/src/main/java/com/spring_boot_template/domain/model/project/Project.java
+++ application/src/main/java/com/spring_boot_template/domain/model/project/Project.java
@@ -1,6 +1,5 @@
 package com.spring_boot_template.domain.model.project;
 
-import com.spring_boot_template.domain.exception.DomainKnowledgeException;
 import com.spring_boot_template.domain.exception.ResourceNotFoundException;
 import com.spring_boot_template.domain.model.account.value.AccountId;
 import com.spring_boot_template.domain.model.project.value.ProjectId;
@@ -11,10 +10,12 @@ import com.spring_boot_template.domain.model.task.value.TaskName;
 import com.spring_boot_template.domain.shared.IdGenerator;
 import java.util.Collections;
 import java.util.LinkedHashSet;
+import java.util.Locale;
 import java.util.Set;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.context.MessageSource;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
@@ -22,25 +23,25 @@ public final class Project {
     private final ProjectId projectId;
     private final ProjectName projectName;
     private final Set<AccountId> accountIds;
-    private final Set<Task> tasks;
+    private final LinkedHashSet<Task> tasks;
 
-    private static final int ASSIGNABLE_TASK_COUNT_FOR_ACCOUNT = 10;
+    private final MessageSource messageSource;
 
     public static Project createProject(
             final IdGenerator idGenerator, final ProjectName projectName) {
         final ProjectId projectId = new ProjectId(idGenerator.generateId());
         final Set<AccountId> accountIds = Collections.emptySet();
-        final Set<Task> tasks = new LinkedHashSet<>();
+        final LinkedHashSet<Task> tasks = new LinkedHashSet<>();
 
-        return new Project(projectId, projectName, accountIds, tasks);
+        return new Project(projectId, projectName, accountIds, tasks, null);
     }
 
     public static Project reconstructProject(
             final ProjectId projectId,
             final ProjectName projectName,
             final Set<AccountId> accountIds,
-            final Set<Task> tasks) {
-        return new Project(projectId, projectName, accountIds, tasks);
+            final LinkedHashSet<Task> tasks) {
+        return new Project(projectId, projectName, accountIds, tasks, null);
     }
 
     public void createTask(final IdGenerator idGenerator, final TaskName taskName) {
@@ -53,7 +54,19 @@ public final class Project {
         return tasks.stream()
                 .filter(task -> task.getTaskId().equals(taskId))
                 .findFirst()
-                .orElseThrow(() -> new ResourceNotFoundException("Task is not found"));
+                .orElseThrow(
+                        () ->
+                                new ResourceNotFoundException(
+                                        messageSource.getMessage(
+                                                "project.task.not-found",
+                                                null,
+                                                Locale.getDefault())));
+    }
+
+    public void deleteTask(final TaskId taskId) {
+        final Task task = findTaskByTaskId(taskId);
+
+        tasks.remove(task);
     }
 
     public void advanceTaskStatus(final TaskId taskId) {
@@ -63,23 +76,8 @@ public final class Project {
     }
 
     public void assignAccountToTask(final TaskId taskId, final AccountId accountId) {
-        final long assignedTaskCountToAccount =
-                tasks.stream()
-                        .filter(task -> task.getAccountIds().contains(accountId))
-                        .filter(task -> task.getStatus().isCountableAsTask())
-                        .count();
-
-        // 割り当てられたタスクの数が限界に達している場合
-        if (assignedTaskCountToAccount >= ASSIGNABLE_TASK_COUNT_FOR_ACCOUNT) {
-            throw new DomainKnowledgeException("Reached assigned Task count limit");
-        }
-
         final Task task = findTaskByTaskId(taskId);
 
         task.assignAccount(accountId);
-    }
-
-    public void deleteTask(final TaskId taskId) {
-        tasks.removeIf(task -> task.getTaskId().equals(taskId));
     }
 }
