1236e3b06a3db4de7867f3602b394a1992b143c7
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-24 17:31:42 +0900

    Update ProjectDto.java

--- application/src/main/java/com/spring_boot_template/infrastructure/project/ProjectDto.java
+++ application/src/main/java/com/spring_boot_template/infrastructure/project/ProjectDto.java
@@ -1,6 +1,5 @@
 package com.spring_boot_template.infrastructure.project;
 
-import com.spring_boot_template.domain.model.account.value.AccountId;
 import com.spring_boot_template.domain.model.project.value.ProjectName;
 
-record ProjectDto(ProjectName projectName, AccountId accountId) {}
+record ProjectDto(ProjectName projectName) {}
