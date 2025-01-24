13200ad37da2f1086e82f64aed15ec005a45f2a6
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-24 17:32:33 +0900

    Update DueDateDetailDto.java

--- application/src/main/java/com/spring_boot_template/infrastructure/due_date_detail/DueDateDetailDto.java
+++ application/src/main/java/com/spring_boot_template/infrastructure/due_date_detail/DueDateDetailDto.java
@@ -1,12 +1,8 @@
 package com.spring_boot_template.infrastructure.due_date_detail;
 
-import com.spring_boot_template.domain.model.due_date_detail.value.DueDate;
 import com.spring_boot_template.domain.model.due_date_detail.value.MaxPostponeCount;
 import com.spring_boot_template.domain.model.due_date_detail.value.PostponeCount;
-import com.spring_boot_template.domain.model.task.value.TaskId;
+import java.sql.Timestamp;
 
 public record DueDateDetailDto(
-        TaskId taskId,
-        DueDate dueDate,
-        PostponeCount postponeCount,
-        MaxPostponeCount maxPostponeCount) {}
+        Timestamp dueDate, PostponeCount postponeCount, MaxPostponeCount maxPostponeCount) {}
