4da1685af9062ee069ad3b70f27740c9be1b411e
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-29 10:29:11 +0900

    Update 02-create-project_account_participations.sql

--- container/database/initdb/02-create-project_account_participations.sql
+++ container/database/initdb/02-create-project_account_participations.sql
@@ -1,11 +1,11 @@
 CREATE TABLE project_account_participations
 (
     project_id VARCHAR(26) REFERENCES projects (project_id) ON DELETE CASCADE,
-    account_id VARCHAR(26) REFERENCES accounts (account_id),
-    PRIMARY KEY (project_id, account_id)
+    participating_account_id VARCHAR(26) REFERENCES accounts (account_id) ON DELETE CASCADE,
+    PRIMARY KEY (project_id, participating_account_id)
 );
 
-INSERT INTO project_account_participations(project_id, account_id)
+INSERT INTO project_account_participations(project_id, participating_account_id)
 VALUES ('0100ABCDEFGHJKMNPQRSTVWXYZ',
         '0000ABCDEFGHJKMNPQRSTVWXYZ'),
        ('0100ABCDEFGHJKMNPQRSTVWXYZ',
