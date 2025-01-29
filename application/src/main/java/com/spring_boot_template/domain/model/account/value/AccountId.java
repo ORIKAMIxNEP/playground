6e44d9043d2058c8037be5df116f9916a05df5a6
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-29 10:35:54 +0900

    Update AccountId.java

--- application/src/main/java/com/spring_boot_template/domain/model/account/value/AccountId.java
+++ application/src/main/java/com/spring_boot_template/domain/model/account/value/AccountId.java
@@ -1,29 +1,3 @@
 package com.spring_boot_template.domain.model.account.value;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import java.util.Objects;
-
-public record AccountId(
-        @NotNull(message = "AccountId is null")
-                @Pattern(
-                        regexp = "^[0123456789ABCDEFGHJKMNPQRSTVWXYZ]{26}$",
-                        message = "AccountId is invalid ULID format")
-                String value) {
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        }
-        if (object == null || getClass() != object.getClass()) {
-            return false;
-        }
-        AccountId accountId = (AccountId) object;
-        return Objects.equals(value, accountId.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(value);
-    }
-}
+public record AccountId(String value) {}
