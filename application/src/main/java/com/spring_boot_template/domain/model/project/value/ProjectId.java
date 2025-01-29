65c28f31161eec2e8c157b5a1f54c009d98e5d95
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-29 11:43:34 +0900

    Update ProjectId.java

--- application/src/main/java/com/spring_boot_template/domain/model/project/value/ProjectId.java
+++ application/src/main/java/com/spring_boot_template/domain/model/project/value/ProjectId.java
@@ -1,11 +1,3 @@
 package com.spring_boot_template.domain.model.project.value;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-
-public record ProjectId(
-        @NotNull(message = "ProjectId is null")
-                @Pattern(
-                        regexp = "^[0123456789ABCDEFGHJKMNPQRSTVWXYZ]{26}$",
-                        message = "ProjectId is invalid ULID format")
-                String value) {}
+public record ProjectId(String value) {}
