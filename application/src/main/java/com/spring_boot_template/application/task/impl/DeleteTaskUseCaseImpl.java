da266e65694c7d0c2bd36698ad54009404bfc697
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-29 11:16:28 +0900

    Update DeleteTaskUseCaseImpl.java

--- application/src/main/java/com/spring_boot_template/application/task/impl/DeleteTaskUseCaseImpl.java
+++ application/src/main/java/com/spring_boot_template/application/task/impl/DeleteTaskUseCaseImpl.java
@@ -5,6 +5,8 @@ import com.spring_boot_template.domain.model.project.Project;
 import com.spring_boot_template.domain.model.project.ProjectRepository;
 import com.spring_boot_template.domain.model.project.value.ProjectId;
 import com.spring_boot_template.domain.model.task.value.TaskId;
+import com.spring_boot_template.presentation.controller.project.request.ProjectIdRequest;
+import com.spring_boot_template.presentation.controller.task.request.TaskIdRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,11 +18,12 @@ class DeleteTaskUseCaseImpl implements DeleteTaskUseCase {
 
     @Override
     @Transactional
-    public void execute(final String projectIdRequest, final String taskIdRequest) {
-        final ProjectId projectId = new ProjectId(projectIdRequest);
+    public void execute(
+            final ProjectIdRequest projectIdRequest, final TaskIdRequest taskIdRequest) {
+        final ProjectId projectId = new ProjectId(projectIdRequest.value());
         final Project project = projectRepository.findProjectByProjectId(projectId);
 
-        final TaskId taskId = new TaskId(taskIdRequest);
+        final TaskId taskId = new TaskId(taskIdRequest.value());
         project.deleteTask(taskId);
         projectRepository.saveProject(project);
     }
