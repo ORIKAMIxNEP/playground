460c4ae943a9a7389359135b5afcfd0aa4302c04
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-29 10:36:16 +0900

    Update AccountRepository.java

--- application/src/main/java/com/spring_boot_template/domain/model/account/AccountRepository.java
+++ application/src/main/java/com/spring_boot_template/domain/model/account/AccountRepository.java
@@ -5,7 +5,7 @@ import com.spring_boot_template.domain.model.account.value.AccountId;
 public interface AccountRepository {
     void saveAccount(final Account account);
 
-    void deleteAccount(final AccountId accountId);
-
     Account findAccountByAccountId(final AccountId accountId);
+
+    void deleteAccount(final AccountId accountId);
 }
