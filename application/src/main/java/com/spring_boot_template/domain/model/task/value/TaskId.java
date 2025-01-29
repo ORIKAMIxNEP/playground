28b91859e2bf4ff168f7f6a2e40c3e5335ccbe32
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-29 11:42:36 +0900

    Update TaskId.java

--- application/src/main/java/com/spring_boot_template/domain/model/task/value/TaskId.java
+++ application/src/main/java/com/spring_boot_template/domain/model/task/value/TaskId.java
@@ -1,11 +1,3 @@
 package com.spring_boot_template.domain.model.task.value;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-
-public record TaskId(
-        @NotNull(message = "TaskId is null")
-                @Pattern(
-                        regexp = "^[0123456789ABCDEFGHJKMNPQRSTVWXYZ]{26}$",
-                        message = "TaskId is invalid ULID format")
-                String value) {}
+public record TaskId(String value) {}
