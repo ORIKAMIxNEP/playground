7858fcadfd06adb06cc35638f19c7805ae58ab30
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-29 10:29:28 +0900

    Update 04-create-task_account_assignments.sql

--- container/database/initdb/04-create-task_account_assignments.sql
+++ container/database/initdb/04-create-task_account_assignments.sql
@@ -1,11 +1,11 @@
 CREATE TABLE task_account_assignments
 (
     task_id    VARCHAR(26) REFERENCES tasks (task_id) ON DELETE CASCADE,
-    account_id VARCHAR(26) REFERENCES accounts (account_id),
-    PRIMARY KEY (task_id, account_id)
+    assigned_account_id VARCHAR(26) REFERENCES accounts (account_id) ON DELETE CASCADE,
+    PRIMARY KEY (task_id, assigned_account_id)
 );
 
-INSERT INTO task_account_assignments(task_id, account_id)
+INSERT INTO task_account_assignments(task_id, assigned_account_id)
 VALUES ('0200ABCDEFGHJKMNPQRSTVWXYZ',
         '0000ABCDEFGHJKMNPQRSTVWXYZ'),
        ('0200ABCDEFGHJKMNPQRSTVWXYZ',
