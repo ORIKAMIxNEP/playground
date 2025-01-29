e992c94665cbcefc52ea5438cce68a826c5db8eb
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-29 11:43:09 +0900

    Update TaskName.java

--- application/src/main/java/com/spring_boot_template/domain/model/task/value/TaskName.java
+++ application/src/main/java/com/spring_boot_template/domain/model/task/value/TaskName.java
@@ -1,12 +1,3 @@
 package com.spring_boot_template.domain.model.task.value;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-
-public record TaskName(
-        @NotNull(message = "TaskName is null")
-                @Size(
-                        min = 1,
-                        max = 10,
-                        message = "TaskName must be between {min} and {max} characters in length")
-                String value) {}
+public record TaskName(String value) {}
