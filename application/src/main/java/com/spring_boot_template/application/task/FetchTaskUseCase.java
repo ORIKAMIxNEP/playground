bc0dbdc2f57ece277f26fe6d59654ae38101294a
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-29 10:45:30 +0900

    Update FetchTaskUseCase.java

--- application/src/main/java/com/spring_boot_template/application/task/FetchTaskUseCase.java
+++ application/src/main/java/com/spring_boot_template/application/task/FetchTaskUseCase.java
@@ -1,7 +1,10 @@
 package com.spring_boot_template.application.task;
 
+import com.spring_boot_template.presentation.controller.project.request.ProjectIdRequest;
+import com.spring_boot_template.presentation.controller.task.request.TaskIdRequest;
 import com.spring_boot_template.presentation.controller.task.response.FetchTaskResponse;
 
 public interface FetchTaskUseCase {
-    FetchTaskResponse execute(final String projectIdRequest, final String taskIdRequest);
+    FetchTaskResponse execute(
+            final ProjectIdRequest projectIdRequest, final TaskIdRequest taskIdRequest);
 }
