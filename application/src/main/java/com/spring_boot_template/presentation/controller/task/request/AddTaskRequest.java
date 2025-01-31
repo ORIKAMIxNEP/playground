a41ca6c28e72a24a74bfbe1faef735bc05cd9767
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-31 17:38:37 +0900

    Update AddTaskRequest.java

--- application/src/main/java/com/spring_boot_template/presentation/controller/task/request/AddTaskRequest.java
+++ application/src/main/java/com/spring_boot_template/presentation/controller/task/request/AddTaskRequest.java
@@ -1,12 +1,16 @@
 package com.spring_boot_template.presentation.controller.task.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Schema(
         title = "追加するタスク",
         requiredProperties = {"taskName"})
 public record AddTaskRequest(
-        @Schema(
+        @NotBlank
+                @Size(min = 1, max = 10)
+                @Schema(
                         title = "タスク名",
                         type = "string",
                         minLength = 1,
