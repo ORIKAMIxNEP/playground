5036415675448ad9c27c10b769c91b58f3703b39
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-31 17:36:26 +0900

    Update DeleteTaskController.java

--- application/src/main/java/com/spring_boot_template/presentation/controller/task/DeleteTaskController.java
+++ application/src/main/java/com/spring_boot_template/presentation/controller/task/DeleteTaskController.java
@@ -1,14 +1,17 @@
 package com.spring_boot_template.presentation.controller.task;
 
 import com.spring_boot_template.application.task.DeleteTaskUseCase;
-import com.spring_boot_template.domain.exception.ResourceNotFoundException;
+import com.spring_boot_template.domain.exception.DomainNotFoundException;
+import com.spring_boot_template.domain.exception.RequestInvalidException;
+import com.spring_boot_template.presentation.controller.project.request.ProjectIdRequest;
+import com.spring_boot_template.presentation.controller.task.request.TaskIdRequest;
+import com.spring_boot_template.presentation.validator.ValidUlid;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,36 +32,16 @@ final class DeleteTaskController {
                         description = "Bad Request",
                         content =
                                 @Content(
-                                        schema =
-                                                @Schema(
-                                                        oneOf = {
-                                                            MethodArgumentNotValidException.class
-                                                        }))),
+                                        schema = @Schema(oneOf = {RequestInvalidException.class}))),
                 @ApiResponse(
                         responseCode = "404",
                         description = "Not Found",
                         content =
-                                @Content(
-                                        schema =
-                                                @Schema(oneOf = {ResourceNotFoundException.class})))
+                                @Content(schema = @Schema(oneOf = {DomainNotFoundException.class})))
             })
     private ResponseEntity<?> execute(
-            @PathVariable
-                    @Schema(
-                            title = "プロジェクトID",
-                            type = "string",
-                            minLength = 26,
-                            maxLength = 26,
-                            example = "0100ABCDEFGHJKMNPQRSTVWXYZ")
-                    final String projectIdRequest,
-            @PathVariable
-                    @Schema(
-                            title = "タスクID",
-                            type = "string",
-                            minLength = 26,
-                            maxLength = 26,
-                            example = "0200ABCDEFGHJKMNPQRSTVWXYZ")
-                    String taskIdRequest) {
+            @PathVariable @ValidUlid final ProjectIdRequest projectIdRequest,
+            @PathVariable @ValidUlid final TaskIdRequest taskIdRequest) {
         deleteTaskUseCase.execute(projectIdRequest, taskIdRequest);
         return ResponseEntity.noContent().build();
     }
