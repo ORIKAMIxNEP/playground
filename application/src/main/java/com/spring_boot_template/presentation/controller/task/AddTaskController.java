2df84995e328f81cbc1669f201c6b67b5e991b85
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-31 17:36:04 +0900

    Update AddTaskController.java

--- application/src/main/java/com/spring_boot_template/presentation/controller/task/AddTaskController.java
+++ application/src/main/java/com/spring_boot_template/presentation/controller/task/AddTaskController.java
@@ -1,25 +1,36 @@
 package com.spring_boot_template.presentation.controller.task;
 
 import com.spring_boot_template.application.task.AddTaskUseCase;
-import com.spring_boot_template.domain.exception.ResourceNotFoundException;
+import com.spring_boot_template.domain.exception.DomainNotFoundException;
+import com.spring_boot_template.domain.exception.RequestInvalidException;
+import com.spring_boot_template.presentation.controller.project.request.ProjectIdRequest;
 import com.spring_boot_template.presentation.controller.task.request.AddTaskRequest;
+import com.spring_boot_template.presentation.validator.ValidUlid;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import java.net.URI;
+import java.util.Locale;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequiredArgsConstructor
 final class AddTaskController {
     private final AddTaskUseCase addTaskUseCase;
+    private final MessageSource messageSource;
 
     @PostMapping(
             value = "/project/{projectIdRequest}/task",
@@ -28,37 +39,38 @@ final class AddTaskController {
             tags = {"task"},
             summary = "タスクを追加する",
             responses = {
-                @ApiResponse(responseCode = "204", description = "No Content"),
+                @ApiResponse(responseCode = "201", description = "Created"),
                 @ApiResponse(
                         responseCode = "400",
                         description = "Bad Request",
                         content =
                                 @Content(
-                                        schema =
-                                                @Schema(
-                                                        oneOf = {
-                                                            MethodArgumentNotValidException.class
-                                                        }))),
+                                        schema = @Schema(oneOf = {RequestInvalidException.class}))),
                 @ApiResponse(
                         responseCode = "404",
                         description = "Not Found",
                         content =
-                                @Content(
-                                        schema =
-                                                @Schema(oneOf = {ResourceNotFoundException.class})))
+                                @Content(schema = @Schema(oneOf = {DomainNotFoundException.class})))
             })
     private ResponseEntity<String> execute(
-            @PathVariable
-                    @Schema(
-                            title = "プロジェクトID",
-                            type = "string",
-                            minLength = 26,
-                            maxLength = 26,
-                            example = "0100ABCDEFGHJKMNPQRSTVWXYZ")
-                    final String projectIdRequest,
-            @RequestBody final AddTaskRequest addTaskRequest) {
-        addTaskUseCase.execute(projectIdRequest, addTaskRequest);
+            @PathVariable @ValidUlid final ProjectIdRequest projectIdRequest,
+            @RequestBody @Validated final AddTaskRequest addTaskRequest,
+            final BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            final FieldError fieldError = bindingResult.getFieldError();
+            if (Objects.nonNull(fieldError)) {
+                final Locale locale = Locale.getDefault();
+                final String message = messageSource.getMessage(fieldError, locale);
+                throw new RequestInvalidException(message);
+            }
+        }
 
-        return ResponseEntity.noContent().build();
+        final String taskId = addTaskUseCase.execute(projectIdRequest, addTaskRequest);
+        final URI location =
+                ServletUriComponentsBuilder.fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(taskId)
+                        .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
