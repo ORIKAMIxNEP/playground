5c93bebd99f78b6fbf11a5f2ff3c4af8990173b7
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-31 17:36:57 +0900

    Update FetchTaskController.java

--- application/src/main/java/com/spring_boot_template/presentation/controller/task/FetchTaskController.java
+++ application/src/main/java/com/spring_boot_template/presentation/controller/task/FetchTaskController.java
@@ -1,9 +1,12 @@
-package com.spring_boot_template.presentation.controller.project;
+package com.spring_boot_template.presentation.controller.task;
 
-import com.spring_boot_template.application.project.FetchProjectsUseCase;
+import com.spring_boot_template.application.task.FetchTaskUseCase;
 import com.spring_boot_template.domain.exception.DomainNotFoundException;
 import com.spring_boot_template.domain.exception.RequestInvalidException;
-import com.spring_boot_template.presentation.controller.project.response.FetchProjectsResponse;
+import com.spring_boot_template.presentation.controller.project.request.ProjectIdRequest;
+import com.spring_boot_template.presentation.controller.task.request.TaskIdRequest;
+import com.spring_boot_template.presentation.controller.task.response.FetchTaskResponse;
+import com.spring_boot_template.presentation.validator.ValidUlid;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -11,19 +14,20 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-final class FetchProjectsController {
-    private final FetchProjectsUseCase fetchProjectsUseCase;
+final class FetchTaskController {
+    private final FetchTaskUseCase fetchTaskUseCase;
 
-    @GetMapping(value = "/projects")
+    @GetMapping(value = "/project/{projectIdRequest}/task/{taskIdRequest}")
     @ResponseBody
     @Operation(
-            tags = {"project"},
-            summary = "プロジェクトリストを取得する",
+            tags = {"task"},
+            summary = "タスクを取得する",
             responses = {
                 @ApiResponse(
                         responseCode = "200",
@@ -32,9 +36,7 @@ final class FetchProjectsController {
                                 @Content(
                                         mediaType = "application/json",
                                         schema =
-                                                @Schema(
-                                                        implementation =
-                                                                FetchProjectsResponse.class))),
+                                                @Schema(implementation = FetchTaskResponse.class))),
                 @ApiResponse(
                         responseCode = "400",
                         description = "Bad Request",
@@ -47,8 +49,9 @@ final class FetchProjectsController {
                         content =
                                 @Content(schema = @Schema(oneOf = {DomainNotFoundException.class})))
             })
-    private ResponseEntity<?> execute() {
-        final String participatingAccountIdRequest = "0000ABCDEFGHJKMNPQRSTVWXYZ";
-        return ResponseEntity.ok(fetchProjectsUseCase.execute(participatingAccountIdRequest));
+    private ResponseEntity<?> execute(
+            @PathVariable @ValidUlid final ProjectIdRequest projectIdRequest,
+            @PathVariable @ValidUlid final TaskIdRequest taskIdRequest) {
+        return ResponseEntity.ok(fetchTaskUseCase.execute(projectIdRequest, taskIdRequest));
     }
 }
