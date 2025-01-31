aded4f742289890e349d0a729ac8c9e76521f8e6
折上 泰生 <65590892+ORIKAMIxNEP@users.noreply.github.com>
2025-01-31 17:36:43 +0900

    Update FetchTaskController.java

--- application/src/main/java/com/spring_boot_template/presentation/controller/task/FetchTaskController.java
+++ application/src/main/java/com/spring_boot_template/presentation/controller/task/FetchTaskController.java
@@ -1,30 +1,29 @@
-package com.spring_boot_template.presentation.controller.task;
+package com.spring_boot_template.presentation.controller.project;
 
-import com.spring_boot_template.application.task.FetchTaskUseCase;
-import com.spring_boot_template.domain.exception.ResourceNotFoundException;
-import com.spring_boot_template.presentation.controller.task.response.FetchTaskResponse;
+import com.spring_boot_template.application.project.FetchProjectsUseCase;
+import com.spring_boot_template.domain.exception.DomainNotFoundException;
+import com.spring_boot_template.domain.exception.RequestInvalidException;
+import com.spring_boot_template.presentation.controller.project.response.FetchProjectsResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-final class FetchTaskController {
-    private final FetchTaskUseCase fetchTaskUseCase;
+final class FetchProjectsController {
+    private final FetchProjectsUseCase fetchProjectsUseCase;
 
-    @GetMapping(value = "/project/{projectIdRequest}/task/{taskIdRequest}")
+    @GetMapping(value = "/projects")
     @ResponseBody
     @Operation(
-            tags = {"task"},
-            summary = "タスクを取得する",
+            tags = {"project"},
+            summary = "プロジェクトリストを取得する",
             responses = {
                 @ApiResponse(
                         responseCode = "200",
@@ -33,42 +32,23 @@ final class FetchTaskController {
                                 @Content(
                                         mediaType = "application/json",
                                         schema =
-                                                @Schema(implementation = FetchTaskResponse.class))),
+                                                @Schema(
+                                                        implementation =
+                                                                FetchProjectsResponse.class))),
                 @ApiResponse(
                         responseCode = "400",
                         description = "Bad Request",
                         content =
                                 @Content(
-                                        schema =
-                                                @Schema(
-                                                        oneOf = {
-                                                            MethodArgumentNotValidException.class
-                                                        }))),
+                                        schema = @Schema(oneOf = {RequestInvalidException.class}))),
                 @ApiResponse(
                         responseCode = "404",
                         description = "Not Found",
                         content =
-                                @Content(
-                                        schema =
-                                                @Schema(oneOf = {ResourceNotFoundException.class})))
+                                @Content(schema = @Schema(oneOf = {DomainNotFoundException.class})))
             })
-    private ResponseEntity<?> execute(
-            @PathVariable
-                    @Schema(
-                            title = "プロジェクトID",
-                            type = "string",
-                            minLength = 26,
-                            maxLength = 26,
-                            example = "0100ABCDEFGHJKMNPQRSTVWXYZ")
-                    final String projectIdRequest,
-            @PathVariable
-                    @Schema(
-                            title = "タスクID",
-                            type = "string",
-                            minLength = 26,
-                            maxLength = 26,
-                            example = "0200ABCDEFGHJKMNPQRSTVWXYZ")
-                    String taskIdRequest) {
-        return ResponseEntity.ok(fetchTaskUseCase.execute(projectIdRequest, taskIdRequest));
+    private ResponseEntity<?> execute() {
+        final String participatingAccountIdRequest = "0000ABCDEFGHJKMNPQRSTVWXYZ";
+        return ResponseEntity.ok(fetchProjectsUseCase.execute(participatingAccountIdRequest));
     }
 }
